8ee95ca872b6ab53bfc375868b757665e8f13a99
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-05-12 22:05:32 -0400

    3-05

---
+++ Unit3-05/index5.ts
@@ -0,0 +1,36 @@
+/*This program uses recursion  with magic squares
+ *By Venika Sem
+ *@version 1.0
+ *@since Feb-2024
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+// tower of hanoi function
+function towerOfHanoi(nOfDisk: number, startPeg: number, endPeg: number) {
+    const pegNumber = 6
+    if (nOfDisk == 1) {
+        console.log(`Move disk 1 from peg ${startPeg} to peg ${endPeg}`)
+        return
+    } else {
+        towerOfHanoi(nOfDisk - 1, startPeg, pegNumber - startPeg - endPeg)
+        console.log(`Move disk ${nOfDisk} from peg ${startPeg} to peg ${endPeg}`)
+        towerOfHanoi(nOfDisk - 1, pegNumber - startPeg - endPeg, endPeg)
+    }
+}
+
+// get user input
+const userInput = createPrompt("How many disks are you moving: ")
+let inputNumber = parseInt(userInput.value)
+
+// make sure the user input a number in the right range
+if (inputNumber < 0) {
+    console.log("\nInput a number over 0.")
+    process.exit()
+}
+
+// run the tower of hanoi function
+const startPeg = 1
+const endPeg = 3
+
+towerOfHanoi(inputNumber, startPeg, endPeg)
