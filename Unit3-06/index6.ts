22f19092d1e51fbeb1ee63ff2160c82379f38568
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-06-05 02:04:09 +0000

    add

--- Unit3-06/index6.ts
+++ Unit3-06/index6.ts
@@ -4,89 +4,63 @@
  * *@since Feb-2024
  * */
 
-// merge sort
-function mergeSort(arr: number[]): number[] {
-    if (arr.length < 2) {
-        return arr
+import { createPrompt } from 'bun-promptx'
+
+/**
+ * Sorts an array using the merging sorting algorithm.
+ *
+ * @param sortArray the array to be sorted
+ * @returns the sorted array
+ */
+function mergeSort(sortArray: number[]): number[] {
+  /**
+   * Merges two arrays together.
+   *
+   * @param left the array to the left
+   * @param right the array to the right
+   * @returns the merged array
+   */
+  function merge(left: number[], right: number[]): number[] {
+    let sortArray: number[] = []
+
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        sortArray.push(left.shift())
+      } else {
+        sortArray.push(right.shift())
+      }
     }
 
-    // math.floor rounds down
-    const middle = Math.floor(arr.length / 2)
-    const left = arr.slice(0, middle)
-    const right = arr.slice(middle)
+    return [...sortArray, ...left, ...right]
+  }
+  const mid = sortArray.length / 2
 
-    return merge(mergeSort(left), mergeSort(right))
+  if (sortArray.length < 2) {
+    return sortArray
+  }
+
+  const left = sortArray.splice(0, mid)
+  return merge(mergeSort(left), mergeSort(sortArray))
 }
 
-// merge
-function merge(left: number[], right: number[]): number[] {
-    let result = []
-    let leftIndex = 0
-    let rightIndex = 0
-
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex])
-            leftIndex++
-        } else {
-            result.push(right[rightIndex])
-            rightIndex++
-        }
-    }
-    // combine the two arrays into a sorted array
-    // concat adds the two arrays together
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+// Create array
+const arrayLength = 5
+let counter = 0
+let array: number[] = []
+while (counter < arrayLength) {
+  const selectedNumber: number = parseInt(createPrompt(`Enter a number to put at [${counter}]: `).value)
+  if (isNaN(selectedNumber)) {
+    console.log("Invalid input.")
+  } else {
+    array.push(selectedNumber)
+    counter++
+  }
 }
 
-// create random arrays with no repeats (lenth 50, 100, 200)
-let arr50: number[] = []
-let arr100: number[] = []
-let arr200: number[] = []
+// Show unsorted array
+console.log(`Unsorted array: ${array}\n`)
 
-// array of 50 random numbers
-for (let counter = 0; counter < 50; counter++) {
-    let num = Math.floor(Math.random() * 50)
-    // make sure no numbers repeat
-    if (!arr50.includes(num)) {
-        arr50.push(num)
-    }
-}
+// Sort array
+console.log(`Sorted array: ${mergeSort(array)}`)
 
-// array of 100 random numbers
-for (let counter = 0; counter < 100; counter++) {
-    let num = Math.floor(Math.random() * 100)
-    // make sure no numbers repeat
-    if (!arr100.includes(num)) {
-        arr100.push(num)
-    }
-}
-
-// array of 200 random numbers
-for (let counter = 0; counter < 200; counter++) {
-    let num = Math.floor(Math.random() * 200)
-    // make sure no numbers repeat
-    if (!arr200.includes(num)) {
-        arr200.push(num)
-    }
-}
-
-// main
-console.log("Original array of 50 random numbers: \n")
-console.log(arr50.join(", "))
-console.log("\nSorted array of 50 random numbers: \n")
-console.log(mergeSort(arr50).join(", "))
-
-console.log("\n**********\n")
-
-console.log("Original array of 100 random numbers: \n")
-console.log(arr100.join(", "))
-console.log("\nSorted array of 100 random numbers: \n")
-console.log(mergeSort(arr100).join(", "))
-
-console.log("\n**********\n")
-console.log("Original array of 200 random numbers: \n")
-console.log(arr200.join(", "))
-console.log("\nSorted array of 200 random numbers: \n")
-console.log(mergeSort(arr200).join(", "))
-
-console.log("\nDone.")
+console.log('\nDone.')
